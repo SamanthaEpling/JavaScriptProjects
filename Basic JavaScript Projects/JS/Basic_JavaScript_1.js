c5de4ca4dbca80639cc92ae76f539879dc9ea0b6
SamanthaEpling <alleycatace@yahoo.com>
2019-11-14 12:26:58 -0800

    Uploading Basic JavaScript Projects folder
    Per Step 58 of JavaScript Course of The Tech Academy; uploading Basic JavaScript Projects folder to JavaScript Projects GitHub repository.  November 14, 2019.

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,19 @@
+var A = "This is fun, this JavaScript business!";
+window.alert(A);
+var B = "Sam says, " 
+    + "\"You couldn\'t have all the fun writing just HTML\\CSS, " 
+    + "now could you!\"";
+var Family = "The Eplings", Dad = "Christopher", Mom = "Samantha", Horse = "Romani", Dog = "Lionel";
+var Dad = Dad.fontcolor("red");
+var Horse = Horse.fontcolor("purple");
+var Dog = Dog.fontcolor("blue");
+document.write(B);
+document.write("Supported by:");
+document.write(Dog, Horse, Dad);
+document.write(6 + 1 + 1 + 2);
+
+function My_First_Function() {
+    var str = "This text is green!";
+    var result = str.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML = result;
+}
